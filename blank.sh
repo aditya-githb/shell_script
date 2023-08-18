1cda422c86ef874da5e125fc136bd13f625f0290
Aditya Kumar Shaw <112172428+GODofGOD007@users.noreply.github.com>
2023-08-18 22:35:33 +0530

    Update blank.sh

--- blank.sh
+++ blank.sh
@@ -1,1 +1,12 @@
 echo "hello world"
+echo "Enter a string: "
+read str 
+
+rev=$(echo $str | rev)
+
+if [ "$str" = "$rev" ]
+then
+	echo "this is palindrome"
+else
+	echo "this is not palindrome"
+fi
