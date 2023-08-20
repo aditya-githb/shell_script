429b394c7f8948b56cb23dfa1938128930d68ecb
Aditya Kumar Shaw <112172428+GODofGOD007@users.noreply.github.com>
2023-08-20 22:29:22 +0530

    Update lana.sh

--- lana.sh
+++ lana.sh
@@ -1,1 +1,18 @@
-read file
+echo "Enter a number: "
+read num
+
+if [ $num -gt '0' ]
+then
+	echo "It is a positive number"
+	
+elif [ $num -lt '0' ]
+then
+	echo "It is a negative number"
+	
+elif [ $num = '0' ]
+then
+	echo "The number is zero"
+
+else
+	echo "You enter a alphabet or something else"
+fi
